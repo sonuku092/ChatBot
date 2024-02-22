5bd7d4373046c3eef1749969bf52f394be69bc61
Sonu Kumar <1AT20IS092@visioneer.atria.edu>
2024-02-22 17:28:23 +0530

    Done

--- frontend/src/components/Chats/Chats.js
+++ frontend/src/components/Chats/Chats.js
@@ -26,6 +26,7 @@ function Chats(props) {
   };
 
   const toggleList = () => {
+    setShowVoice(false)
     setShowList(!showList);
   };
 
@@ -37,37 +38,46 @@ function Chats(props) {
     <div className="h-screen">
       <div className="w-full px-14 py-1 bg-slate-400">
         <div className="flex gap-1">
-          <div className="flex-none px-10 max-h-max bg-slate-50 rounded-sm py-2 items-center">
+          <div className="flex-none px-10 max-h-max  rounded-sm py-2 items-center">
             <h1>Chatbot</h1>
           </div>
-          <div className="flex-grow max-h-max bg-slate-50 rounded-sm py-2">
+          <div className="flex-grow max-h-max  rounded-sm py-2">
             <button onClick={handleSignout}>Logout</button>
           </div>
-          <div className="flex w-auto pl-8 py-1 max-h-max bg-slate-50 rounded-sm items-center">
+          <div className="flex w-auto pl-8 py-1 max-h-max  rounded-sm items-center">
             <h2>{userName ? `Welcome - ${userName}` : "Please Login"}</h2>
             <div className="w-6 h-6 rounded-[50%] border-[1px] mx-2 border-black"></div>
           </div>
         </div>
       </div>
 
-      <div className="w-full h-[93vh] bg-green-400 flex">
-        <div className={`w-${showList ? '1/5' : '0'} border-2 transition-all duration-500 overflow-hidden`}>
+      <div className="w-full h-[93vh] flex">
+
+        <div className={`w-${showList ? '1/4' : '0'} border-2 transition-all duration-500 overflow-hidden `}>
           <div className="border-b-2 pb-2">
-            {showList && <button onClick={toggleList}>Toggle List</button>}
-            List
+            {showList && 
+            <div className=" w-[200px]">
+              List
+            </div>}
           </div>
         </div>
 
-        <div className="w-full border-2">
+        <div className="w-auto border-2 flex-grow">
           <h1>Main Content</h1>
           <button onClick={toggleList}>Toggle List</button>
           <button onClick={toggleVoice}>Toggle Voice</button>
+          {showList && <div>List content...</div>}
         </div>
 
-        <div className={`w-${showVoice ? '1/5' : '0'} md:block border-2 transition-all duration-500 overflow-hidden`}>
-          {showVoice && <button onClick={toggleVoice}>Toggle Voice</button>}
-          Voice
+        <div className={`w-${showVoice ? 'fit' : '0'} md:block border-2 transition-all duration-2000 overflow-hidden`}>
+          <div className="border-b-2 pb-2">
+            {showVoice && 
+            <div className=" w-[400px]">
+              Voice
+            </div>}
+          </div>
         </div>
+
       </div>
     </div>
   );
