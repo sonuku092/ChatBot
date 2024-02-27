adfb8d632e9022acb7c065b142ea02b1bc9bde64
Sonu Kumar <1AT20IS092@visioneer.atria.edu>
2024-02-27 13:10:52 +0530

    Backend

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,23 @@
-// server.js
-const express = require('express');
-const http = require('http');
-const socketIo = require('socket.io');
 
+import  express  from "express";
+import { Server } from "socket.io";
+import { createServer } from "http";
+
+const port = 3000;
 const app = express();
-const server = http.createServer(app);
-const io = socketIo(server);
+const server = createServer(app)
+const io = new Server(server);
 
-const PORT = process.env.PORT || 3000;
-
-io.on('connection', (socket) => {
-    console.log('A user connected');
-
-    socket.on('disconnect', () => {
-        console.log('User disconnected');
-    });
-
-    socket.on('chat message', (msg) => {
-        console.log('message: ' + msg);
-        io.emit('chat message', msg);
-    });
+app.get('/', (req, res) => {
+    res.send('Hello World');
 });
 
-server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+io.on('connection', (server) => {
+    console.log('a user connected');
+    console.log(server.id);
+});
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
