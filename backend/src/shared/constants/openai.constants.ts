0dfe45af3caeb4679485754e99decb079f1c8807
Sonu Kumar <1AT20IS092@visioneer.atria.edu>
2024-03-15 18:08:37 +0530

    Done

--- backend/src/shared/constants/openai.constants.ts
+++
@@ -1,6 +0,0 @@
-// export const OPENAI_API_KEY = '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';
-
-export const OpenAIConstants = {
-    apiKey: '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',
-    apiUrl: 'https://api.openai.com/v1',
-};
--- backend/src/shared/dto/user-message.dto.ts
+++
@@ -1,4 +0,0 @@
-export class UserMessageDto {
-    message: string;
-  }
-  
