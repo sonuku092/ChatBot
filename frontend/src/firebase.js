f772f7589d30a163d278d058c791299d0746aa9a
Sonu Kumar <1AT20IS092@visioneer.atria.edu>
2024-03-16 17:41:34 +0530

    fire base

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,5 +1,4 @@
 import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from 'firebase/firestore';
 
@@ -14,10 +13,9 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
 
 const auth = getAuth(app);  
 
 const db = getFirestore(app);
 
-export { app, auth, db };
+export {db, auth };
